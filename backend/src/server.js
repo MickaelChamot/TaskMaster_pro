ad45ff2b5f5b54632e1bce5d6dfbb2b384c734bd
Mickael Chamot <skymic69@hotmail.fr>
2026-03-01 20:59:36 +0100

    add authRoutes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import pool from "./config/database.js";
+import router from "./routes/authRoutes.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth", router);
 
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
