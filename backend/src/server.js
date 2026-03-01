ade12cbba7b5b8bb06bf87d19cb1e750723b6225
Mickael Chamot <skymic69@hotmail.fr>
2026-03-01 19:33:48 +0100

    add database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import pool from "./config/database.js";
 
 const app = express();
 
@@ -8,6 +9,11 @@ app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.listen(3000, () => {
-  console.log("server is runnig");
+app.listen(3000, async () => {
+  try {
+    await pool.query("SELECT 1");
+    console.log("Serveur démarré et base de donné connectée ✅ ");
+  } catch (error) {
+    console.log("Erreur de connetion à la base de donnée ❌");
+  }
 });
