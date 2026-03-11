3a19925a3d266d3fb68ab69a9b894ec9a66df9f4
Mickael Chamot <skymic69@hotmail.fr>
2026-03-11 21:43:58 +0100

    Setup React frontend wutih routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Dashboard from "./pages/dashboard";
+
 function App() {
   return (
-    <div>
-      <h1>TaskMaster Pro</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
